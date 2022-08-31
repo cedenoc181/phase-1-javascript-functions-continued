f028955e2a013722fb5c349ce33f08a117ded70e
Christian <cedenoc181@gmail.com>
2022-08-31 11:57:12 -0400

    commit tree 8fcf3afdfa97bca5761d89c74b806ae6a5a47869

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+function saturdayFun(){
+    console.log("This Saturday, I want to roller-skate!")
+}
+saturdayFun();
+
+
+function saturdayFun(activity = "roller-skate!") {
+    console.log(`This Saturday, I want to ${activity}`);
+}
+saturdayFun();
+saturdayFun("bathe my dog!");
